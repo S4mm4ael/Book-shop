e1899e6f2b0a46e279f5db46b8ae4a1e0a96201e
S4mm4el <homer1996@gmail.com>
2023-02-23 15:57:44 +0300

    feat: add count of books to nav

--- src/components/navigation-list/category-item/category-item.tsx
+++ src/components/navigation-list/category-item/category-item.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useLocation, useParams } from 'react-router-dom';
 
-import { AppDispatch } from '../../../redux/store';
-import { Category } from '../../../shared/types.books';
+import { useGetAllBooksQuery } from '../../../redux/features/books-slice';
+import { AppDispatch, RootState } from '../../../redux/store';
+import { defineRoute } from '../../../shared/define-ru-category';
+import { Book, Category } from '../../../shared/types.books';
 
 import styles from '../navigation-list.module.css';
 
-export function CategoryItem({ name, path, id }: Category) {
+export function CategoryItem({ name, path }: Category) {
   const location = useLocation();
   const dispatch: AppDispatch = useDispatch();
+
+  const { data: books = [], error, isLoading } = useGetAllBooksQuery('');
 
   function handleClick() {
     dispatch({ type: 'IS_BURGER_OPEN', payload: false })
     dispatch({ type: 'CATEGORY', payload: path })
+  }
+
+  function renderCount() {
+    let count = 0;
+
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].categories.includes(name)) {
+        count += 1;
+      }
+    }
+
+    return <span>{count}</span>
+
   }
 
   return (
@@ -34,7 +51,7 @@ export function CategoryItem({ name, path, id }: Category) {
             : styles.NavigationList__booksItemCount_standart
           }`}
       >
-        {id}
+        {renderCount()}
       </span>
     </React.Fragment>
   );
