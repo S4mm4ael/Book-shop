3f77a2d5889d53a9390609dd1914d84425a2df96
S4mm4el <homer1996@gmail.com>
2023-03-03 19:11:31 +0300

    feat: add html/css for the Auth form

--- src/pages/auth/auth-page.tsx
+++ src/pages/auth/auth-page.tsx
@@ -6,6 +6,20 @@ type AuthProps = {
 };
 
 export function AuthPage({ contentView }: AuthProps) {
-  return <section className={styles.Auth}>auth-page</section>;
+  return <section className={styles.Auth}>
+    <div className={styles.Auth__formContainer}>
+      <h1 className={styles.Auth__title}>Вход в личный кабинет</h1>
+      <form className={styles.Auth__form} action="">
+        <input className={styles.Auth__formItem} type="text" placeholder='Логин' />
+        <input className={styles.Auth__formItem} type="text" placeholder='Пароль' />
+        <p className={styles.Auth__formForgot}>Забыли логин или пароль?</p>
+        <button className={styles.Auth__formButton} type="submit"  >Вход</button>
+      </form>
+    </div>
+
+
+
+  </section >
+
 }
 
