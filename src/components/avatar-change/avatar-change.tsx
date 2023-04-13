ee9d00db93456ce5b7919f67de642294c78ffb3c
S4mm4el <homer1996@gmail.com>
2023-04-13 17:30:29 +0300

    feat: add image chaching for avatar

--- src/components/avatar-change/avatar-change.tsx
+++ src/components/avatar-change/avatar-change.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import avatar from '../../assets/png/avatar-big.png';
 import intersect from '../../assets/png/avatar-intersect.png';
@@ -9,16 +9,62 @@ import styles from './avatar-change.module.css';
 export const AvatarChange = () => {
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
+  function setAvatarToLocalStore() {
+    localStorage.removeItem('thumbnail');
+    if (selectedImage) {
+      const userAvatar = selectedImage;
+      const reader = new FileReader();
+
+      reader.readAsDataURL(userAvatar);
+      reader.addEventListener('load', () => {
+        if (reader.result) {
+          localStorage.setItem('thumbnail', String(reader.result));
+        }
+      });
+    }
+  }
+
+  function dataURLtoFile(dataUrl: string, filename: string) {
+    if (!selectedImage) {
+      const url = dataUrl;
+
+      fetch(url)
+        .then((res) => res.blob())
+        .then((blob) => setSelectedImage(new File([blob], filename, { type: 'image/png' })))
+        .then(() => {
+          console.log('uploaded');
+        });
+    }
+  }
+
+  function getAvatarFromLocalStore() {
+    const thumbnail = localStorage.getItem('thumbnail');
+
+    if (thumbnail) {
+      dataURLtoFile(thumbnail, 'chachedAvatar');
+    } else {
+      console.log('nothing');
+    }
+  }
+
+  useEffect(() => {
+    getAvatarFromLocalStore();
+  });
+
   return (
     <div>
-
       <div className={styles.Profile_avatarContainer}>
-        <label className={styles.Profile__inputLabel} htmlFor="image-selector">
+        <label className={styles.Profile__inputLabel} htmlFor='image-selector'>
           <img className={styles.Profile__avatarIntersect} src={intersect} alt='avatar-change' />
           <img className={styles.Profile__avatarIcon} height={32} src={icon} alt='avatar-icon' />
-          <img className={styles.Profile__avatar} alt='not found' width="160px" height="160px" src={selectedImage ? URL.createObjectURL(selectedImage) : avatar} />
+          <img
+            className={styles.Profile__avatar}
+            alt='not found'
+            width='160px'
+            height='160px'
+            src={selectedImage ? URL.createObjectURL(selectedImage) : avatar}
+          />
         </label>
-
 
         <input
           type='file'
@@ -28,13 +74,11 @@ export const AvatarChange = () => {
           onChange={(event) => {
             if (event && event.target && event.target.files) {
               setSelectedImage(event.target.files[0]);
+              setAvatarToLocalStore();
             }
-
           }}
         />
-
       </div>
-
-    </div >
+    </div>
   );
 };
